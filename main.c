d008e957bf054cf4d6540e25799c6f8090efd139
Lennart Martens <monkeynator78@gmail.com>
2024-08-08 18:45:52 +0200

    Update
    It looks better now ;-)

--- main.c
+++ main.c
@@ -11,10 +11,12 @@
 #include <arpa/inet.h>
 #include "selfie.c"
 
-#define KB 1024
-#define MB (KB * KB)
-#define GB (MB * KB)
-#define HOUR (60 * 60)
+#define KB      1024
+#define MB      (KB * KB)
+#define GB      (MB * KB)
+#define MINUTE  60
+#define HOUR    (MINUTE * MINUTE)
+#define DAY     (HOUR * 24)
 #define BLUE    "\x1b[34m"
 #define RESET   "\x1b[0m"
 #define RED     "\x1b[31m"
@@ -46,9 +48,17 @@ void printSystemInfo() {
     FILE *osRelease = fopen("/etc/os-release", "r");
     if (osRelease != NULL) {
         char line[256];
+        char c;
         while (fgets(line, sizeof(line), osRelease)) {
             if (strncmp(line, "PRETTY_NAME=", 12) == 0) {
-                printf(BOLD CYAN "OS" RESET ":%s", line + 12); // Skip "PRETTY_NAME="
+                for(int i = 12; i < sizeof(line); i++) // Remove the quotes
+                {
+                    if(line[i] == '"')
+                    {
+                        line[i] = ' ';
+                    }
+                }
+                printf(BOLD CYAN "OS" RESET ":%s", line + 13); // Skip "PRETTY_NAME="
                 break;
             }
         }
@@ -58,13 +68,13 @@ void printSystemInfo() {
     }
 
     // Desktop Environment
-    char *desktopEnv = getenv("XDG_CURRENT_DESKTOP");
+    char *desktopEnv = getenv("XDG_CURRENT_DESKTOP"); 
     if (desktopEnv != NULL) {
         printf(BOLD CYAN "Desktop Environment" RESET ":%s\n", desktopEnv);
     } else {
         printf(BOLD CYAN "Desktop Environment" RESET ":Unknown\n");
     }
-
+    
     // CPU
     FILE *cpuInfo = fopen("/proc/cpuinfo", "r");
     if (cpuInfo != NULL) {
@@ -97,33 +107,59 @@ void printSystemInfo() {
     }
 }
 
+char* get_uptime(long uptime_seconds)
+{
+    char* uptime_str = malloc(100);
+    uptime_str == NULL ? NULL : 0;
+    
+    if (uptime_seconds > DAY)
+    {
+        sprintf(uptime_str, "%.2f days", (double)uptime_seconds / DAY);
+    }
+    else if (uptime_seconds > HOUR)
+    {
+        sprintf(uptime_str, "%.2f hours", (double)uptime_seconds / HOUR);
+    }
+    else if (uptime_seconds > MINUTE)
+    {
+        sprintf(uptime_str, "%.2f minutes", (double)uptime_seconds / MINUTE);
+    }
+    else
+    {
+        sprintf(uptime_str, "%ld s", uptime_seconds);
+    }
+    
+    return uptime_str;
+}
 
 int main() {
-    printf(ansi_pic);
     struct sysinfo info;
-    printf(GREEN);
-    printf("  ||==================||\n");
-    printf("-~||System Information||~-\n");
-    printf("  ||==================||\n");
-    printf("  ||                  ||\n");
+
+    printf(ansi_pic);
+
+    printf(BOLD GREEN"\n");
+    printf("==================\n");
+    printf("System Information\n");
+    printf("==================\n");
+
     printf(RESET);
+
     printSystemInfo();
 
+    (sysinfo(&info) != 0) ? (perror("sysinfo"), 1) : 0;
+    
+    char *uptime = get_uptime(info.uptime);
 
-    if (sysinfo(&info) == 0) {
-        printf(BOLD);
-        printf(BOLD CYAN "Uptime"RESET":%-.3f h\n", (double)info.uptime / HOUR);
-        printf(BOLD CYAN "Total RAM"RESET":%-.3f GB\n", (double)info.totalram / GB);
-        printf(BOLD CYAN "Free RAM"RESET":%-.3f GB\n", (double)(info.freeram / GB));
-        printf(BOLD CYAN "Shared RAM"RESET":%-.3f GB\n", (double)info.sharedram / GB);
-        printf(BOLD CYAN "Buffered RAM"RESET":%-.3f GB\n", (double)info.bufferram / GB);
-        printf(BOLD CYAN "Total Swap"RESET":%-.3f GB\n", (double)info.totalswap / GB);
-        printf(BOLD CYAN "Free Swap"RESET":%-.3f GB\n", (double)info.freeswap / GB);
-        printf(BOLD CYAN "Number of processes:"RESET"%d\n", info.procs);
-    } else {
-        perror("sysinfo");
-    }
-    printf(RESET);
+    printf(BOLD CYAN "Uptime"RESET":%s\n", uptime);
+    printf(BOLD CYAN "Total RAM"RESET":%.2f GiB\n", (double)info.totalram / GB);
+    printf(BOLD CYAN "Free RAM"RESET":%.2f GiB\n", (double)(info.freeram / GB));
+    printf(BOLD CYAN "Shared RAM"RESET":%.2f GiB\n", (double)info.sharedram / GB);
+    printf(BOLD CYAN "Buffered RAM"RESET":%.2f GiB\n", (double)info.bufferram / GB);
+    printf(BOLD CYAN "Total Swap"RESET":%.2f GiB\n", (double)info.totalswap / GB);
+    printf(BOLD CYAN "Free Swap"RESET":%.2f GiB\n", (double)info.freeswap / GB);
+    printf(BOLD CYAN "Number of processes:"RESET"%d\n", info.procs);    
+
+    free(uptime);
 
     return 0;
 }
